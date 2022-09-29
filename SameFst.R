a2f9847c7d8f72cca63d0d10f48d4a6277e253a4
dyerlab <dyerlab@icloud.com>
2022-09-29 15:23:08 -0400

    daily, got one locus fst/ht going, adding three allele setup for one locus.

--- SameFst.R
+++ SameFst.R
@@ -5,43 +5,96 @@ library( tidyverse )
 
 
 
-random_loci <- function( p, sd = 0.1, N = 40 ) {
-  q <- (1-p) + rnorm(1,sd=sd)
-  freq <- data.frame( Allele = c("A","B"),
-                      Frequency = c(1-q, q ))
-  return( make_loci( freq, N = N) )
+df <- data.frame( sd = rep(seq( 0.1, 0.2, by = 0.01), each=100),
+                  fst = NA)
+
+
+for( i in 1:nrow(df) ) { 
+  sd <- df$sd[i]
+  p <- runif(2, min=0.2, max=0.8 )
+  f1 <- rnorm( 5, mean=p[1], sd=sd )
+  f1[ f1 < 0 ] <- 0 
+  f1[ f1 > 1 ] <- 1 
+  f <- matrix( c(f1, 1-f1), nrow=5)
+  ht <- 1 - sum((colSums(f)/nrow(f))^2)
+  hs <- mean(1-rowSums(f^2))
+  fst <- (ht - hs)/ht
+  df$fst[i] <- ifelse( fst < 1.0, fst, 1.0 )
 }
+
+
+df %>%
+  mutate( sd = factor( sd, ordered=TRUE ) ) %>%
+  group_by( sd ) %>%
+  summarize( Min = min(fst),
+             Mean = mean( fst ),
+             Max = max( fst ))
+
+
+
+
+sd <- 0.18
+
+for( i in 1:10000 ) { 
   
-random_pops <- function( nloci = 2, npops = 5, ninds = 50, sd = 0.1) { 
-  ret <- data.frame() 
-  p <- runif( nloci,min = 0.2, max = 0.8 )
-  for( i in 1:npops ) { 
-    pop <- data.frame( Stratum = rep( LETTERS[i], ninds) )
-    for( j in 1:nloci) {
-      locName <- paste( "loc",j,sep=".")
-      pop[[locName]] <- random_loci(p[j], sd, ninds)
-    }
-    ret <- rbind( ret, pop )
+  p1 <- rnorm( 5, mean=runif(1, min=0.2, max=0.8 ), sd=sd )
+  p2 <- rnorm( 5, mean=runif(1, min=0.2, max=0.8 ), sd=sd )
+  
+  if( any(p1 + p2 > 1.0) ) {
+    p1 <- p1 / 2
+    p2 <- p2 / 2 
   }
-  return( ret )
+  p3 <- 1 - (p1 + p2)
+  f <- matrix( c(p1,p2,p3), nrow=5)
+  ht <- 1 - sum((colSums(f)/nrow(f))^2)
+  hs <- mean(1-rowSums(f^2))
+  fst <- (ht - hs)/ht
+  if( ht > 0  ) { 
+    if( fst < 0.051 & fst > 0.05 ) {
+      tmp <- c(fst, p1, p2)
+      out <- as.data.frame(matrix( tmp, nrow=1))
+      write_csv(out, 
+                file="data/fst_0.1.csv",
+                append = TRUE,
+                col_names = FALSE)
+      print( tmp )
+    }  
+  }
 }
 
-loci <- rep( c("Loc.1", "Loc.2"), times=10)
-stratum <- rep(paste("Pop",1:5, sep="."), times = 2 )
 
 
+read_csv( "data/fst_0.1.csv") %>%
+  mutate( ht = 2*pt*(1-pt) + 2* ) %>%
+  arrange( fst, ht ) %>% View() 
+  
+  
+  mutate( pt = (p1+p2+p3+p4+p5)/5 ) %>%
+  
+  filter( fst < 0.1009,
+          ht > 0.444,
+          ht < 0.445) %>%
+  arrange(  ht ) %>% 
+  select( fst, ht, everything()) -> freq_spectra
 
-p <- abs(runif( 1 ) + rnorm(10,sd=0.5) )
-q <- 1-p
-data.frame( Stratum = stratum, 
-            Locus = loci, 
-            Allele = c( rep("A",10), rep("B", 10)),
-            Frequency = c(p,q) ) %>%
-  arrange( Stratum, Locus, Allele ) -> f 
-make_population( f, N = 100 ) -> pop
-genetic_structure( pop )
+save(freq_spectra, file="data/freq_spectra.rda")
 
 
+df <- data.frame( PopSet = rep(1:nrow(freq_spectra ), each=5) )
+df$fst <- rep( freq_spectra$fst, each=5 )
+df$ht <- rep( freq_spectra$ht, each=5)
+df$p <- c( as.numeric( freq_spectra[1,3:7]),
+           as.numeric( freq_spectra[2,3:7]),
+           as.numeric( freq_spectra[3,3:7]),
+           as.numeric( freq_spectra[4,3:7]),
+           as.numeric( freq_spectra[5,3:7]),
+           as.numeric( freq_spectra[6,3:7]),
+           as.numeric( freq_spectra[7,3:7]),
+           as.numeric( freq_spectra[8,3:7]),
+           as.numeric( freq_spectra[9,3:7]),
+           as.numeric( freq_spectra[10,3:7]),
+           as.numeric( freq_spectra[11,3:7])
+           )
+df$PopSet <- factor( df$PopSet, ordered  = TRUE)
 
-
-
+ggplot( df, aes(p, 1-p) ) + geom_point() 
