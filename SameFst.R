87e155f77bd69f626d62562cc40a7e964827e604
Rodney J. Dyer <rjdyer@vcu.edu>
2022-09-30 08:46:12 -0400

    git the standardized values set

--- SameFst.R
+++ SameFst.R
@@ -2,36 +2,7 @@ rm( list = ls() )
 
 library( gstudio )
 library( tidyverse )
-
-
-
-df <- data.frame( sd = rep(seq( 0.1, 0.2, by = 0.01), each=100),
-                  fst = NA)
-
-
-for( i in 1:nrow(df) ) { 
-  sd <- df$sd[i]
-  p <- runif(2, min=0.2, max=0.8 )
-  f1 <- rnorm( 5, mean=p[1], sd=sd )
-  f1[ f1 < 0 ] <- 0 
-  f1[ f1 > 1 ] <- 1 
-  f <- matrix( c(f1, 1-f1), nrow=5)
-  ht <- 1 - sum((colSums(f)/nrow(f))^2)
-  hs <- mean(1-rowSums(f^2))
-  fst <- (ht - hs)/ht
-  df$fst[i] <- ifelse( fst < 1.0, fst, 1.0 )
-}
-
-
-df %>%
-  mutate( sd = factor( sd, ordered=TRUE ) ) %>%
-  group_by( sd ) %>%
-  summarize( Min = min(fst),
-             Mean = mean( fst ),
-             Max = max( fst ))
-
-
-
+library( gganimate )
 
 sd <- 0.18
 
@@ -51,50 +22,70 @@ for( i in 1:10000 ) {
   fst <- (ht - hs)/ht
   if( ht > 0  ) { 
     if( fst < 0.051 & fst > 0.05 ) {
-      tmp <- c(fst, p1, p2)
-      out <- as.data.frame(matrix( tmp, nrow=1))
-      write_csv(out, 
-                file="data/fst_0.1.csv",
-                append = TRUE,
-                col_names = FALSE)
-      print( tmp )
+      if( ht < 0.651 & ht > 0.65 ) {
+        tmp <- c(fst, ht, p1, p2)
+        out <- as.data.frame(matrix( tmp, nrow=1))
+        write_csv(out, 
+                  file="data/fst_0.1.csv",
+                  append = TRUE,
+                  col_names = FALSE)
+        print( tmp )
+      }
     }  
   }
 }
 
+tmp <- read_csv("data/fst_0.1.csv")
+
+df <- data.frame( Set = rep(1:12, each = 5),
+                  Locale = rep(LETTERS[1:5], times = 12),
+                  Fst = rep( tmp$fst, each = 5), 
+                  Ht = rep( tmp$ht, each = 5 ))
+df$p1 <- c( as.numeric(tmp[1,3:7]),
+            as.numeric(tmp[2,3:7]),
+            as.numeric(tmp[3,3:7]),
+            as.numeric(tmp[4,3:7]),
+            as.numeric(tmp[5,3:7]),
+            as.numeric(tmp[6,3:7]),
+            as.numeric(tmp[7,3:7]),
+            as.numeric(tmp[8,3:7]),
+            as.numeric(tmp[9,3:7]),
+            as.numeric(tmp[10,3:7]),
+            as.numeric(tmp[11,3:7]),
+            as.numeric(tmp[12,3:7]) )
+            
+df$p2 <- c( as.numeric(tmp[1,8:12]),
+            as.numeric(tmp[2,8:12]),
+            as.numeric(tmp[3,8:12]),
+            as.numeric(tmp[4,8:12]),
+            as.numeric(tmp[5,8:12]),
+            as.numeric( tmp[6,8:12]),
+            as.numeric(tmp[7,8:12]),
+            as.numeric(tmp[8,8:12]),
+            as.numeric(tmp[9,8:12]),
+            as.numeric(tmp[10,8:12]),
+            as.numeric(tmp[11,8:12]),
+            as.numeric(tmp[12,8:12]) )
+
+summary( df )
 
 
-read_csv( "data/fst_0.1.csv") %>%
-  mutate( ht = 2*pt*(1-pt) + 2* ) %>%
-  arrange( fst, ht ) %>% View() 
-  
-  
-  mutate( pt = (p1+p2+p3+p4+p5)/5 ) %>%
-  
-  filter( fst < 0.1009,
-          ht > 0.444,
-          ht < 0.445) %>%
-  arrange(  ht ) %>% 
-  select( fst, ht, everything()) -> freq_spectra
-
-save(freq_spectra, file="data/freq_spectra.rda")
+df %>%
+  filter( Set == 1) %>%
+  mutate( label1 = paste( "Fst = ", format(mean(Fst),digits=8)),
+          label2 = paste( "Ht = ", format( mean(Ht), digits=8)) ) %>%
+  ggplot( aes(p1, p2 ) ) + 
+    geom_point( size=4 ) + 
+    theme_minimal() +
+    xlim( c(0,1) )  + 
+    ylim( c(0,1) ) + 
+    geom_text( aes(0.75, 0.75, label=label1) )
+    
 
 
-df <- data.frame( PopSet = rep(1:nrow(freq_spectra ), each=5) )
-df$fst <- rep( freq_spectra$fst, each=5 )
-df$ht <- rep( freq_spectra$ht, each=5)
-df$p <- c( as.numeric( freq_spectra[1,3:7]),
-           as.numeric( freq_spectra[2,3:7]),
-           as.numeric( freq_spectra[3,3:7]),
-           as.numeric( freq_spectra[4,3:7]),
-           as.numeric( freq_spectra[5,3:7]),
-           as.numeric( freq_spectra[6,3:7]),
-           as.numeric( freq_spectra[7,3:7]),
-           as.numeric( freq_spectra[8,3:7]),
-           as.numeric( freq_spectra[9,3:7]),
-           as.numeric( freq_spectra[10,3:7]),
-           as.numeric( freq_spectra[11,3:7])
-           )
-df$PopSet <- factor( df$PopSet, ordered  = TRUE)
 
-ggplot( df, aes(p, 1-p) ) + geom_point() 
++ 
+    transition_states( Set ) + 
+    ease_aes('linear')
+
+
